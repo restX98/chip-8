1e9570ee18fa4ebf384a8b013e10ad81d93bb6b6
Enrico Restuccia <enrico.rest98@gmail.com>
2025-08-10 16:18:22 +0100

    Dummy window with SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,68 @@
-#include "SDL3/SDL.h"
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main() {
-    SDL_Log("%s", "Hello, Chip-8 Emulator!");
-    return 0;
+#define APP_NAME "Chip-8 Emulator"
+#define APP_VERSION "1.0"
+#define APP_IDENTIFIER "com.emulators.chip8"
+
+#define WINDOW_RESIZABLE false
+
+#define SCALE 30
+#define WIDTH 64
+#define HEIGHT 32
+
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window* window = nullptr;
+static SDL_Renderer* renderer = nullptr;
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+    SDL_SetAppMetadata(APP_NAME, APP_VERSION, APP_IDENTIFIER);
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/primitives", WIDTH * SCALE, HEIGHT * SCALE, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetWindowResizable(window, WINDOW_RESIZABLE);
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void* appstate) {
+    SDL_FRect rect;
+
+    /* as you can see from this, rendering draws over whatever was drawn before it. */
+    SDL_SetRenderDrawColor(renderer, 33, 33, 33, SDL_ALPHA_OPAQUE);  /* dark gray, full alpha */
+    SDL_RenderClear(renderer);  /* start with a blank canvas. */
+
+    /* draw a filled rectangle in the middle of the canvas. */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  /* blue, full alpha */
+    rect.x = rect.y = 100;
+    rect.w = 10;
+    rect.h = 10;
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);  /* put it all on the screen! */
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    /* SDL will clean up the window/renderer for us. */
 }
